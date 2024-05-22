88f8f777edd0d7ddf156fc8fce5797c7d7eaa712
birachis <birumoa@gmail.com>
2024-05-23 01:48:16 +0300

    esc 2

--- escrow.sol
+++ escrow.sol
@@ -5,10 +5,15 @@ contract Escrow {
 	address public arbiter;
 	address public beneficiary;
 	address public depositor;
-	constructor(address _arbiter, address _beneficiary){
+
+	constructor(address _arbiter, address _beneficiary) payable {
 		arbiter = _arbiter;
 		beneficiary = _beneficiary;
 		depositor = msg.sender;
 	}
-	
+
+	function approve() external {
+		(bool success, ) = beneficiary.call{ value: address(this).balance }("");
+		require(success);
+	}
 }
