adb06076034923a959e19d8462e23bb4086a422f
birachis <birumoa@gmail.com>
2024-05-23 14:40:41 +0300

    inh 2

--- applied solidity/inheritance.sol
+++ applied solidity/inheritance.sol
@@ -3,6 +3,14 @@ pragma solidity 0.8.20;
 
 import "./Hero.sol";
 
-contract Warrior is Hero { }
+contract Warrior is Hero(200) { 
+    function attack(Enemy enemy) public override {
+        enemy.takeAttack(Hero.AttackTypes.Brawl);
+    }
+}
 
-contract Mage is Hero { }
+contract Mage is Hero(50) { 
+    function attack(Enemy enemy) public override {
+        enemy.takeAttack(Hero.AttackTypes.Spell);
+    }
+}
