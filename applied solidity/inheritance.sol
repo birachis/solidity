028d50155b4a9d0b7b7a5893c12b36cb9609cbc2
birachis <birumoa@gmail.com>
2024-05-23 14:42:06 +0300

    inh fin

--- applied solidity/inheritance.sol
+++ applied solidity/inheritance.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "./Hero.sol";
-
-contract Warrior is Hero(200) { 
-    function attack(Enemy enemy) public override {
-        enemy.takeAttack(Hero.AttackTypes.Brawl);
-    }
+contract Ownable {
+    constructor() { owner = msg.sender; }
+    address owner;
+	modifier onlyOwner {
+		require(msg.sender == owner);
+		_;
+	}
 }
 
-contract Mage is Hero(50) { 
-    function attack(Enemy enemy) public override {
-        enemy.takeAttack(Hero.AttackTypes.Spell);
-    }
+contract Transferable is Ownable {
+	function transfer(address newOwner) external onlyOwner {
+		owner = newOwner;
+	}
 }
